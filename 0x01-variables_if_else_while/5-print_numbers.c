79e50b20ed67fb3fee64658a3ee7f1d46e1599be
Goslowvich <corneliusgoslowvich@gmail.com>
2022-12-11 22:45:02 -0800

    tikka

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing base 10 numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int base;
+
+	base = 10;
+	for (base = 0; base < 10; base)
+	printf("%d", base);
+	printf("\n");
+	return (0);
+}
