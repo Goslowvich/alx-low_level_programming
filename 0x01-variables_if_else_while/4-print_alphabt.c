8edc1f84891cfd0a2fd1e0c11cba132396fe9bc9
Goslowvich <corneliusgoslowvich@gmail.com>
2022-12-11 22:32:02 -0800

    world

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing a-z without q&e
+ * Return: always 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+	if (az != 'e' && az != 'q')
+	putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
