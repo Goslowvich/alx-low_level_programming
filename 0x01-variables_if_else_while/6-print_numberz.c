39085cc3bc97a14eda4095bf47c821c411fbc6bc
Goslowvich <corneliusgoslowvich@gmail.com>
2022-12-11 23:23:36 -0800

    hi

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing base 10 numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
