71829c1b05429ca859ad898353adc93c8482ea08
Goslowvich <corneliusgoslowvich@gmail.com>
2022-12-13 11:55:33 -0800

    tekkaa

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * _putchar that will be followed a new line
+ *
+ * Return: Always 0
+ */
+int(void);
+{
+	_putchaar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('/n');
+	return (0);
+}
